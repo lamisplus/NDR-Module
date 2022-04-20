6e79d86153aa3128906f19db9a86fc4fabed6795
aniwange33 <terteseamos@gmail.com>
2022-04-20 16:22:16 +0100

    feat: reactor repository

--- src/main/java/org/lamisplus/modules/hiv/repository/HivEnrollmentRowMapper.java
+++ src/main/java/org/lamisplus/modules/hiv/repository/HivEnrollmentRowMapper.java
@@ -11,27 +11,25 @@ public class HivEnrollmentRowMapper implements RowMapper<HivEnrollmentDTO> {
     @Override
     public HivEnrollmentDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
         HivEnrollmentDTO hivEnrollmentDTO = new HivEnrollmentDTO ();
-        hivEnrollmentDTO.setId(rs.getLong("id"));
-                hivEnrollmentDTO.setArchived(rs.getInt("archived"));
-                hivEnrollmentDTO.setBreastfeeding(rs.getBoolean("breastfeeding"));
-                hivEnrollmentDTO.setDateConfirmedHiv(rs.getDate("date_confirmed_hiv").toLocalDate ());
-                hivEnrollmentDTO.setDateEnrolledPMTCT(rs.getDate("date_enrolled_pmtct").toLocalDate ());
-                hivEnrollmentDTO.setDateOfRegistration(rs.getDate("date_of_registration").toLocalDate ());
-                hivEnrollmentDTO.setDateStarted(rs.getDate("date_started").toLocalDate ());
-                hivEnrollmentDTO.setEnrollmentSettingId(rs.getLong("enrollment_setting_id"));
-                hivEnrollmentDTO.setEntryPointId(rs.getLong("entry_point_id"));
-                //hivEnrollmentDTO.setExtra(rs.getObject("extra").toString());
-                hivEnrollmentDTO.setPersonId(rs.getLong("person_id"));
-                hivEnrollmentDTO.setPregnant(rs.getBoolean("pregnant"));
-                hivEnrollmentDTO.setSendMessage(rs.getBoolean("send_message"));
-                hivEnrollmentDTO.setSourceOfReferrer(rs.getString("source_of_referrer"));
-                hivEnrollmentDTO.setStatusAtRegistrationId(rs.getLong("status_at_registration_id"));
-                hivEnrollmentDTO.setTargetGroupId(rs.getLong("target_group_id"));
-                hivEnrollmentDTO.setTbStatusId(rs.getLong("tb_status_id"));
-               // hivEnrollmentDTO.setTimeHivDiagnosis(LocalDateTime.from(rs.getTime("time_hiv_diagnosis").toLocalTime()));
-                hivEnrollmentDTO.setUniqueId(rs.getString("unique_id"));
-                hivEnrollmentDTO.setUuid(rs.getString("uuid"));
+        hivEnrollmentDTO.setId (rs.getLong ("id"));
+        hivEnrollmentDTO.setArchived (rs.getInt ("archived"));
+        hivEnrollmentDTO.setBreastfeeding (rs.getBoolean ("breastfeeding"));
+        hivEnrollmentDTO.setDateConfirmedHiv (rs.getDate ("date_confirmed_hiv").toLocalDate ());
+        hivEnrollmentDTO.setDateEnrolledPMTCT (rs.getDate ("date_enrolled_pmtct").toLocalDate ());
+        hivEnrollmentDTO.setDateOfRegistration (rs.getDate ("date_of_registration").toLocalDate ());
+        hivEnrollmentDTO.setDateStarted (rs.getDate ("date_started").toLocalDate ());
+        hivEnrollmentDTO.setEnrollmentSettingId (rs.getLong ("enrollment_setting_id"));
+        hivEnrollmentDTO.setEntryPointId (rs.getLong ("entry_point_id"));
+        hivEnrollmentDTO.setPersonId (rs.getLong ("person_id"));
+        hivEnrollmentDTO.setPregnant (rs.getBoolean ("pregnant"));
+        hivEnrollmentDTO.setSendMessage (rs.getBoolean ("send_message"));
+        hivEnrollmentDTO.setSourceOfReferrer (rs.getString ("source_of_referrer"));
+        hivEnrollmentDTO.setStatusAtRegistrationId (rs.getLong ("status_at_registration_id"));
+        hivEnrollmentDTO.setTargetGroupId (rs.getLong ("target_group_id"));
+        hivEnrollmentDTO.setTbStatusId (rs.getLong ("tb_status_id"));
+        hivEnrollmentDTO.setUniqueId (rs.getString ("unique_id"));
+        hivEnrollmentDTO.setUuid (rs.getString ("uuid"));
 
-                return hivEnrollmentDTO;
+        return hivEnrollmentDTO;
     }
 }
