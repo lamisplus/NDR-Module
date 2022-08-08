596d608c65a3c98e9571aeca83cf5e14bf91f86a
aniwange33 <terteseamos@gmail.com>
2022-08-08 13:38:50 +0100

    fix:extend person dto to hiv

--- src/main/java/org/lamisplus/modules/hiv/domain/dto/HivPatientDto.java
+++ src/main/java/org/lamisplus/modules/hiv/domain/dto/HivPatientDto.java
@@ -1,44 +1,17 @@
 package org.lamisplus.modules.hiv.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.lamisplus.modules.patient.utility.LocalDateConverter;
+import org.lamisplus.modules.patient.domain.dto.PersonResponseDto;
 
-import javax.persistence.Convert;
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-public class HivPatientDto implements Serializable {
-    private Long id;
-    private Long visitId;
-    private Boolean active;
-    private String surname;
-    private String firstName;
-    private String otherName;
-    private Object gender;
-    private Boolean deceased;
-    private Object maritalStatus;
-    private Object employmentStatus;
-    private Object education;
-    private Object organization;
-    @Convert(converter = LocalDateConverter.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate dateOfBirth;
-    private LocalDateTime deceasedDateTime;
-    private Object identifier;
-    private Object contact;
-    private Object contactPoint;
-    private Object address;
-    private LocalDate dateOfRegistration;
-    private Boolean IsDateOfBirthEstimated;
+public class HivPatientDto extends PersonResponseDto  implements Serializable {
     private boolean isEnrolled;
     private boolean isCommenced;
     private boolean isClinicalEvaluation;
     private boolean isMentalHealth;
-    private boolean biometricStatus;
     private String currentStatus;
     private HivEnrollmentDto enrollment;
     private ARTClinicalCommenceDto artCommence;
