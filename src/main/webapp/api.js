c9440f6fc3d62f41bb46d31afeb1810ba23b3507
gichangi <gichangijohn3@gmail.com>
2022-09-07 19:02:16 +0300

    backup

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,7 @@
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
 
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
+/*
 export const url =  'http://localhost:8282/api/v1/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYxODQ5NDYyfQ.NH8pABNJxgT1R0mvV3XsmrZmWndECDvkUxzJD2HKU9ttxBtJKLgIcIx6okrNA-fMqcDc6Se1KJmbXXzfV_TrrA';
+*/
