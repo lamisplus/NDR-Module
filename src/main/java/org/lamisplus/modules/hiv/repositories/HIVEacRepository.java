093f5312cbbaa8c4ed026b3984a4c98a74a730fc
aniwange33 <terteseamos@gmail.com>
2022-09-14 11:12:05 +0100

    feat: implement  domain entities

--- src/main/java/org/lamisplus/modules/hiv/repositories/HIVEacRepository.java
+++ src/main/java/org/lamisplus/modules/hiv/repositories/HIVEacRepository.java
@@ -1,19 +1,15 @@
 package org.lamisplus.modules.hiv.repositories;
 
-//import org.lamisplus.modules.hiv.domain.entity.HIVEac;
-//import org.lamisplus.modules.patient.domain.entity.Person;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//import java.util.Optional;
+import org.lamisplus.modules.hiv.domain.entity.HIVEac;
+import org.lamisplus.modules.patient.domain.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface HIVEacRepository extends JpaRepository<HIVEac, Long> {
-//
-//    List<HIVEac> getAllByPersonAndArchived(Person person, Integer archived);
-//    Optional<HIVEac> getByPersonAndDateOfEac2IsNullAndDateOfEac3IsNull(Person person);
-//    Optional<HIVEac> getByPersonAndDateOfEac3IsNull(Person person);
-//    Optional<HIVEac> getByPersonAndDateOfEac1IsNotNullAndDateOfEac2IsNotNullAndDateOfEac3IsNull(Person person);
-//
-//
-//
-//}
+import java.util.List;
+
+
+public interface HIVEacRepository extends JpaRepository<HIVEac, Long> {
+    List<HIVEac> getAllByPersonAndArchived(Person person, Integer archived);
+	
+
+
+}
--- src/main/java/org/lamisplus/modules/hiv/repositories/HIVEacSessionRepository.java
+++ src/main/java/org/lamisplus/modules/hiv/repositories/HIVEacSessionRepository.java
@@ -1,7 +1,7 @@
 package org.lamisplus.modules.hiv.repositories;
 
-//import org.lamisplus.modules.hiv.domain.entity.HIVEacSession;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface HIVEacSessionRepository extends JpaRepository<HIVEacSession, Long> {
-//}
+import org.lamisplus.modules.hiv.domain.entity.HIVEacSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HIVEacSessionRepository extends JpaRepository<HIVEacSession, Long> {
+}
