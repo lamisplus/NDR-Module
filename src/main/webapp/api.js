75b3266e1a98850f6adcd32729cb3e91e8ec1440
aniwange33 <terteseamos@gmail.com>
2022-09-27 07:39:32 +0100

    feat: EAC and EAC Session provider implemented

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
-export const url =  'http://localhost:8383/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzOTcyNzA5fQ.ratq0ni-wqIsyL84A6My-iHzc2TZn3s_wqJL5mAWU1HfzD6Zmf4F6HibR2TlnGbxRospYTWQf86dc32KKlwbsw';
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+//export const url =  'http://localhost:8383/api/v1/';
+//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzOTcyNzA5fQ.ratq0ni-wqIsyL84A6My-iHzc2TZn3s_wqJL5mAWU1HfzD6Zmf4F6HibR2TlnGbxRospYTWQf86dc32KKlwbsw';
