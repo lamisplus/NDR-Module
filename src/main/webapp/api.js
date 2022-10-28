6aac3234e01a44b56adae21079421647cfdb3159
Mathew Adegbite <mathewadegbite@gmail.com>
2022-10-28 17:30:28 +0100

    updating code

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
-export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY2OTc2NTU1fQ.C_xinvkNpDWUlVBZQee2FdA8Z1Xi-khQuicIZzb4RKxLoRFGabHTFCRIc4uX-X1u_6pR0lGdj0bp5ik9MGWTLQ';
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+// export const url =  'http://localhost:8282/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY2OTc2NTU1fQ.C_xinvkNpDWUlVBZQee2FdA8Z1Xi-khQuicIZzb4RKxLoRFGabHTFCRIc4uX-X1u_6pR0lGdj0bp5ik9MGWTLQ';
