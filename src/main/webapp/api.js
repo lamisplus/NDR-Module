f7a1babac113bb5e73337277f79874b00ca0c8ca
Mathew Adegbite <mathewadegbite@gmail.com>
2022-11-08 14:07:26 +0100

    recent history

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-// export const url =  'http://localhost:8282/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY3OTUwNTMyfQ.rcHOx9chgqloeqmwPwKNFhYg-J85Pv17cQWZSc4NsJX_uNaW3R1n9mfX92jRw-CRuXNrHYrNHZPRJDJMM18aeg';
+// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+// export const url = '/api/v1/'
+export const url =  'http://localhost:8282/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY3OTUwNTMyfQ.rcHOx9chgqloeqmwPwKNFhYg-J85Pv17cQWZSc4NsJX_uNaW3R1n9mfX92jRw-CRuXNrHYrNHZPRJDJMM18aeg';
