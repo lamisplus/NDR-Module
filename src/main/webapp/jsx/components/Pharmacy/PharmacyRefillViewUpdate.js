b9be0711fd069e57c1586b6088695433591978d6
Mathew Adegbite <mathewadegbite@gmail.com>
2023-01-03 13:46:03 +0100

    visit type on pharmacy

--- src/main/webapp/jsx/components/Pharmacy/PharmacyRefillViewUpdate.js
+++ src/main/webapp/jsx/components/Pharmacy/PharmacyRefillViewUpdate.js
@@ -115,7 +115,8 @@ const Pharmacy = (props) => {
             dsdModelType,
             switch:"",
             substitute:"",
-            iptType:""
+            iptType:"",
+            visitType:''
     });
     const [vital, setVitalSignDto]= useState({
         bodyWeight: "",
@@ -814,6 +815,25 @@ const Pharmacy = (props) => {
                 </div>
             </>)}
             <div className="row">
+            <div className="form-group mb-3 col-md-3">
+                <FormGroup>
+                    <Label >Visit Type</Label>
+                    <Input
+                        type="select"
+                        name="visitType"
+                        id="visitType"
+                        value={objValues.visitType}
+                        onChange={handleInputChange}   
+                        style={{border: "1px solid #014D88", borderRadius:"0.25rem"}}                 
+                        >
+                        <option value="">Select </option>
+                        <option value="Initial Visit">Initial Visit</option>
+                        <option value="Follow Up Visit">Follow Up Visit </option>
+                    
+                    </Input>
+                    
+                    </FormGroup>
+                </div>
                 <div className="form-group mb-3 col-md-3">
                 <FormGroup>
                     <Label >Refill</Label>
