b92be3de3edac7c731f642dbea544396615a7c5b
Mathew77 <mathewadegbite@gmail.com>
2023-01-24 09:36:11 +0100

    tpt type fix

--- src/main/webapp/jsx/components/Pharmacy/PharmacyRefillViewUpdate.js
+++ src/main/webapp/jsx/components/Pharmacy/PharmacyRefillViewUpdate.js
@@ -1202,7 +1202,7 @@ const Pharmacy = (props) => {
             {showIptType || objValues.iptType!=='' && ( 
             <div className="form-group mb-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <FormGroup>
-                <Label >TPT Type</Label>
+                <Label >Visit Type</Label>
                 
                 <Input
                     type="select"
