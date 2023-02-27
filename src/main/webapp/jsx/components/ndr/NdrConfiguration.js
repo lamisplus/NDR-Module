1880169e65102471e19c70cbeb8d67a5588cedba
Mathew77 <mathewadegbite@gmail.com>
2023-02-27 18:15:53 +0100

    fixing setup bugs

--- src/main/webapp/jsx/components/ndr/NdrConfiguration.js
+++ src/main/webapp/jsx/components/ndr/NdrConfiguration.js
@@ -138,10 +138,11 @@ export default function DownloadNdr() {
         setModal(!modal)
     }
 
+    
     return (
         <div>
             <ToastContainer autoClose={3000} hideProgressBar />
-            {ndrDetail!=="" &&  [ndrDetail].length <=0 && (
+            {ndrDetail===undefined && (
             <Button
                 variant="contained"
                 color="primary"
@@ -180,8 +181,8 @@ export default function DownloadNdr() {
                         <Dropdown item text='Action'>
 
                         <Dropdown.Menu style={{ marginTop:"10px", }}>
-                            <Dropdown.Item  onClick={() =>setupModal(row)}><Edit color="primary"/> Edit
-                            </Dropdown.Item>
+                            {/* <Dropdown.Item  onClick={() =>setupModal(row)}><Edit color="primary"/> Edit
+                            </Dropdown.Item> */}
                             <Dropdown.Item  onClick= {() => deleteModal(row.id)}><DeleteOutline color="primary"/> Delete
                             </Dropdown.Item>
                         </Dropdown.Menu>
@@ -228,7 +229,7 @@ export default function DownloadNdr() {
             
         </Modal2.Footer>
         </Modal2>      
-            <NdrConfigurationSetup toggleModal={toggleModal} showModal={showModal} NdrSetup={ndrSetup} setUpDetail={setUpDetail}/>
+            <NdrConfigurationSetup toggleModal={toggleModal} showModal={showModal} NdrSetup={ndrSetup} setUpDetail={ndrDetail}/>
         </div>
 
     );
