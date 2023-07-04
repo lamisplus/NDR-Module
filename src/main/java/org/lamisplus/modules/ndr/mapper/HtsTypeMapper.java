cb2477a593cc14235788ae7eeec446471b3999e8
aniwange33 <terteseamos@gmail.com>
2023-07-04 10:23:03 +0100

    feat: start implementation of HTS1

--- src/main/java/org/lamisplus/modules/ndr/mapper/HtsTypeMapper.java
+++ src/main/java/org/lamisplus/modules/ndr/mapper/HtsTypeMapper.java
@@ -2,9 +2,12 @@ package org.lamisplus.modules.ndr.mapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.lamisplus.modules.ndr.domain.dto.PatientDemographicDTO;
 import org.lamisplus.modules.ndr.schema.HIVTestingReportType;
 import org.springframework.stereotype.Component;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 
 @Component
@@ -17,6 +20,18 @@ public class HtsTypeMapper {
 		HIVTestingReportType hivTestingReportType = NDRObjectFactory.createHIVTestingReportType();
 		//demographicDTO.getPersonUuid();
 		//demographicDTO.get
+		
+//		@XmlElement(name = "ClientCode", required = true)
+//		protected String clientCode;
+//		@XmlElement(name = "VisitDate", required = true)
+//		@XmlSchemaType(name = "date")
+//		protected XMLGregorianCalendar visitDate;
+//		@XmlElement(name = "VisitID", required = true)
+//		protected String visitID;
+//		@XmlElement(name = "Setting", required = true)
+//		protected String setting;
+//		@XmlElement(name = "FirstTimeVisit", required = true)
+//		protected String firstTimeVisit;
 		
 		return hivTestingReportType;
 	}
