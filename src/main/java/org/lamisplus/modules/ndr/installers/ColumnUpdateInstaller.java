db7a38188981e9376a0c08e589a299ed874faec4
amos <terteseamos@gmail.com>
2023-08-07 15:32:48 +0100

    fix: add sql query to update tb status display

--- src/main/java/org/lamisplus/modules/ndr/installers/ColumnUpdateInstaller.java
+++ src/main/java/org/lamisplus/modules/ndr/installers/ColumnUpdateInstaller.java
@@ -6,7 +6,7 @@ import org.springframework.core.annotation.Order;
 
 @Order(2)
 @Installer(name = "column-update_installer",
-        description = "add new column to NDR related tables", version = 1)
+        description = "add new column to NDR related tables", version = 2)
 public class ColumnUpdateInstaller extends AcrossLiquibaseInstaller {
     public ColumnUpdateInstaller() {
         super("classpath:installers/ndr/schema/colum_update.xml");
