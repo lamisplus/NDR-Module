dc49e4fa2c37e466cd1e83f719b0b71dc440e14f
vixxjy <ajorvictor48@gmail.com>
2024-02-27 13:56:20 +0100

    [NDR] Updates made generating treatment NDR

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url = "/api/v1/";
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4OTkyMzg4fQ.noxAsvN1t0kBk-QsJ-JqFDn0iUekydWi9S_psYcI_GQrqWtFmn7dgfBHHBT50kpihWihN0Px73imEcLbGTY8Dg";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDc2MjAyfQ.w-5TkRpgiLMWBj-3R1iRK-Q3B4SiJOSlblMEPYwfAJu7NEPGBT0eAGKwAU1LVKnpB1-pkVzuiVO_w4Gmv20rbQ";
 */
