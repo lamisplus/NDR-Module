5aa92d59dd927498918e02c8522df1af5afcb814
vixxjy <ajorvictor48@gmail.com>
2024-02-27 19:10:27 +0100

    [NDR] Updates made for api

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,9 @@
-/*
+
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-*/
 
+/*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA5MDk2Mzg5fQ.J62H-_xLaGo7rXXI0V0xu6w_29fxBPVBNvXYOq3iSpZMwU_bcVwfVA58QIcOXC7hbtjwlSh_8nOpC34ArBcm1Q";
+*/
