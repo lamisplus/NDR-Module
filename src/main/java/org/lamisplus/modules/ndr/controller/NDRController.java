80b55c50f1d31664591658ee0fc2c5271e19ff49
Aniwange Tertese Amos <terteseamos@gmail.com>
2024-02-28 10:38:06 +0100

    fix: null encounters for updated patients

--- src/main/java/org/lamisplus/modules/ndr/controller/NDRController.java
+++ src/main/java/org/lamisplus/modules/ndr/controller/NDRController.java
@@ -72,6 +72,7 @@ public class NDRController {
             @RequestParam List<String> patientIds){
         messagingTemplate.convertAndSend("/topic/ndr-status", "start");
         Stopwatch stopwatch = Stopwatch.createStarted();
+        System.out.println("calling patients api with initial value  = " + initial);
         facilityIds.forEach (facilityId -> ndrOptmizationService.generateNDRXMLByFacilityAndListOfPatient(facilityId,initial,patientIds));
         messagingTemplate.convertAndSend("/topic/ndr-status", "end");
         log.info("Total time taken to generate a file: {}", stopwatch.elapsed().toMillis());
